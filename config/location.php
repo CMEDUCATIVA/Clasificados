c9d929ce489dd0603d35a6358df18fa96125ad63
CMEDUCATIVA <161378183+CMEDUCATIVA@users.noreply.github.com>
2026-04-28 18:26:08 -0500

    Update dataset local path and URLs in location.php

--- config/location.php
+++ config/location.php
@@ -2,15 +2,16 @@
 
 return [
     'seed' => [
+        'prefer_remote' => (bool) env('LOCATION_SEED_PREFER_REMOTE', true),
         'dataset_local_path' => env(
             'LOCATION_SEED_DATASET_LOCAL_PATH',
-            'location/countries-states-cities-database-master/json/countries+states+cities.json'
+            'location/countries+states+cities.json'
         ),
         'dataset_timeout_seconds' => (int) env('LOCATION_SEED_DATASET_TIMEOUT_SECONDS', 180),
         'dataset_urls' => array_values(array_filter([
-            env('LOCATION_SEED_DATASET_URL_1', ''),
-            env('LOCATION_SEED_DATASET_URL_2', ''),
-            env('LOCATION_SEED_DATASET_URL_3', ''),
+            env('LOCATION_SEED_DATASET_URL_1', 'https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/json/countries+states+cities.json'),
+            env('LOCATION_SEED_DATASET_URL_2', 'https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/json/countries+states.json'),
+            env('LOCATION_SEED_DATASET_URL_3', 'https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/json/countries.json'),
         ])),
     ],
 ];

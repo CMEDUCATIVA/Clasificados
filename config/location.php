f6dec222fb2dc5ca54ac7bee184291f03ddf2752
CMEDUCATIVA <161378183+CMEDUCATIVA@users.noreply.github.com>
2026-04-28 19:19:18 -0500

    Update location.php

--- config/location.php
+++ config/location.php
@@ -2,7 +2,6 @@
 
 return [
     'seed' => [
-        'prefer_remote' => (bool) env('LOCATION_SEED_PREFER_REMOTE', true),
         'memory_limit' => env('LOCATION_SEED_MEMORY_LIMIT', '1024M'),
         'minimum_countries' => (int) env('LOCATION_SEED_MINIMUM_COUNTRIES', 200),
         'dataset_local_path' => env(
@@ -10,11 +9,6 @@ return [
             'location/countries+states+cities.json'
         ),
         'dataset_timeout_seconds' => (int) env('LOCATION_SEED_DATASET_TIMEOUT_SECONDS', 180),
-        'dataset_urls' => array_values(array_filter([
-            env('LOCATION_SEED_DATASET_URL_1', 'https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/json/countries+states+cities.json'),
-            env('LOCATION_SEED_DATASET_URL_2', 'https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/json/countries+states.json'),
-            env('LOCATION_SEED_DATASET_URL_3', 'https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/json/countries.json'),
-        ])),
     ],
 ];
 
